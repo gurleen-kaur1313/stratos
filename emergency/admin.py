4ee1737dec07a0fd95edbc01a42c96c936aca6b5
Gurleen <gurleendhillonm1114@gmail.com>
2021-02-28 07:18:49 +0530

    announcement done

--- emergency/admin.py
+++ emergency/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import HealthEmergency,HealthTest
+from .models import HealthEmergency,HealthTest,Announcement
 
 admin.site.register(HealthEmergency)
 admin.site.register(HealthTest)
+admin.site.register(Announcement)
